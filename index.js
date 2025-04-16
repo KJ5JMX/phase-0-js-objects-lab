285fe62c12e8692b886c557175319a28d5876c16
Blake Godsey <somethingclever182@gmail.com>
2025-04-15 19:04:44 -0500

    sneaky sneaky adding something outside of the instructions... gave functions to modify added objects for ticket pricing

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 //Write your code here
 
+const  attendee  = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150,
+}
+
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+} 
+
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+  console.log("Ticket type updated to: " + newTicketType);
+}
+
+updateTicketPrice = (attendee, newTicketPrice) => {
+  attendee.ticketPrice = newTicketPrice;
+  console.log("Ticket price updated to: " + newTicketPrice);
+}
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+  console.log("Event property removed.");
+} 
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+  console.log("Checked-in property added.");
+}     
+
 
 
 //Needed for the tests to work. Don't modify
